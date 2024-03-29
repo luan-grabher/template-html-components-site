8e2a516d3dce3601390dcd4b297653b217302aeb
luangrabher <luanggcontato@gmail.com>
2024-03-28 22:13:48 -0300

    feat - copia todos arquivos .css para a dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js', // Arquivo de entrada principal
+  entry: "./src/index.js", // Arquivo de entrada principal
   output: {
-    path: path.resolve(__dirname, 'dist'), // Diretório de saída para os arquivos buildados
-    filename: 'bundle.js' // Nome do arquivo JavaScript buildado
+    path: path.resolve(__dirname, "dist"), // Diretório de saída para os arquivos buildados
+    filename: "bundle.js", // Nome do arquivo JavaScript buildado
   },
   module: {
     rules: [
       {
         test: /\.html$/, // Regra para arquivos HTML
         use: [
-          path.resolve(__dirname, './html-loader.js'), // Caminho para o seu carregador
-        ]
-      }
-    ]
+          path.resolve(__dirname, "./html-loader.js"), // Caminho para o seu carregador
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Arquivo HTML de origem
-      filename: 'index.html' // Nome do arquivo HTML buildado
+      template: "./src/index.html", // Arquivo HTML de origem
+      filename: "index.html", // Nome do arquivo HTML buildado
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/styles.css', to: 'styles.css' }, // ajuste o caminho de origem e destino conforme necessário
+        {
+          from: "src/*.css",
+          to: ({ context, absoluteFilename }) => {
+            return path.basename(absoluteFilename);
+          },
+        },
       ],
     }),
-  ]
+  ],
 };
