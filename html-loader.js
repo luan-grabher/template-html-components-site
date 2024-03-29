d3de4599beb63aa8d0a8160cd5882e402db2c336
luangrabher <luanggcontato@gmail.com>
2024-03-28 21:49:54 -0300

    feat - chama recursivamente o loader de templates até que todos templates tenham sido susbtituidos.

--- html-loader.js
+++ html-loader.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function(content) {
+function processTemplates(content, resourcePath) {
   const regex = /<template src="(.+?)"><\/template>/g;
   let match;
   let newContent = content;
 
   while ((match = regex.exec(content)) !== null) {
-    const filePath = path.resolve(path.dirname(this.resourcePath), match[1]);
+    const filePath = path.resolve(path.dirname(resourcePath), match[1]);
     const fileContent = fs.readFileSync(filePath, 'utf8');
-    newContent = newContent.replace(match[0], fileContent);
+    newContent = newContent.replace(match[0], processTemplates(fileContent, filePath));
   }
 
-  return `module.exports = ${JSON.stringify(newContent)}`;
+  return newContent;
+}
+
+module.exports = function(content) {
+  const processedContent = processTemplates(content, this.resourcePath);
+  return `module.exports = ${JSON.stringify(processedContent)}`;
 };
